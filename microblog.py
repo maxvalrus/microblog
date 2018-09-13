e0c78c28517820249bbd9935c67c3857b8100fa8
maxvalrus <maxval.rus@gmail.com>
2018-09-13 16:08:12 +0300

    db

--- microblog.py
+++ microblog.py
@@ -1,4 +1,9 @@
-from app_mod import app
+from app_mod import app, db
+from app_mod.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db':db, 'User':User, 'Post':Post}
 
 if __name__ == "__main__":
     app.run()
